7ccb50d4fcfc7f130d54476816fd4dff98f30cb9
Masquerade Circus <christian@masquerade-circus.net>
2018-12-01 14:41:53 -0600

    Improve home ui.

--- app/ui/pages/home.js
+++ app/ui/pages/home.js
@@ -51,24 +51,39 @@ let Page = {
                             <select onchange={Page.selectWorld}>
                                 {Page.getOptions()}
                             </select>
-                            <button
-                                onclick={Page.play}
-                                data-background="success"
-                                disabled={!Game.is.ready || Game.is.connecting}
-                                class="w100">
-                                {Game.is.connecting ? 'Connecting...' : Game.is.ready ? 'Play' : 'Loading...'}
-                            </button>
+                            <div class="text-center">
+                                <button
+                                    onclick={Page.play}
+                                    data-background="success"
+                                    disabled={!Game.is.ready || Game.is.connecting}
+                                    class="w75">
+                                    {Game.is.connecting ? 'Connecting...' : Game.is.ready ? 'Play' : 'Loading...'}
+                                </button>
+                            </div>
+
                         </form>
-                    </Panel>
-                    <Panel position="inline" color="black">
-                        <div class="text-justify">
-                            <h3>How to play</h3>
-                            Collect resources to buy better equipement and gold to be the number one.
-                            <br/><br/><h3>Controls</h3>
-                            <code>WASD</code> To move
+                        <br/>
+                        <div class="text-justify" style="height: 250px; overflow: auto">
+                            <b>How to play</b>
+                            <br/>Collect resources to buy better equipment and gold to be the number one.
+                            <br/>
+                            <br/><b>Controls</b>
+                            <br/><code>WASD</code> To move
                             <br/><code>Mouse</code> To look
                             <br/><code>Left button</code> To gather resources / fight other players
                             <br/><code>Mousewheel/1-4</code> To equip item
+                            <br/>
+                            <br/><b>Strategies</b>
+                            <br/>If you have enough resources you can buy weapons and you can increase its effectivity by upgrading from stone to iron, silver and gold levels.  With the correct resources the store will automatically appear and show the items you can buy.
+                            <br/><br/>Sheeps will give you food and gold.
+                            <br/><br/>Knife is fast to buy and upgrade, it gives you a little more range and probability to gather resources and ​a little more fight power.
+                            <br/><br/>Axe will give you medium range and more probability to gather resources and medium fight power.
+                            <br/><br/>Spear will give you the largest range but no probability increase to gather resources and high fight power.
+                            <br/>
+                            <br/><b>Roadmap</b>
+                            <br/>[ ] - ​Add more animals (birds, lion, wild boar)
+                            <br/>[ ] - Add more items (bow, sword)
+                            <br/>[ ] - Add items to build your house (walls, door, traps)
                         </div>
                     </Panel>
                 </div>
